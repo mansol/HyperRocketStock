a2d3e582d6f47df5daf172ca16e43314bc39ff0d
mansol <1867632+mansol@users.noreply.github.com>
2017-07-26 21:14:21 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -45,56 +45,37 @@ public class Main extends FragmentActivity {
 
     private static final String TAG = Main.class.getSimpleName();
     private ViewGroup mListSelectorLayout, mDetailLayout;
-
     public static final int CONTENT_VIEW_ID3 = 101010111;
-
     static boolean has_panes;
-
     private static boolean flag2 = false;
-
     private static final int XML = 0;
-    private static final int JSON = 1;
-    
+    private static final int JSON = 1; //JSON and XML, not functional yet, only CSV.   
     private static final int CSV = 3;
     private int mode = CSV; //default
     //private int mode= JSON;
-
     private static String loser = null;
     private static Double mostNeg = 0.0;
     private static String winner = null;
     private static Double mostPos = 0.0;
-
     private static String mostAction = null;
     private static Double maxAveVol = 0.0;
-
     public static Stock[] pubStocks;
-
     private TextView symbolsList2;
-
-    public ListView listView;
-    
+    public ListView listView;  
     public static int absolute = 0;
-
     ListFragment listFragment;
-
     private static String[] symbols;
     private static String[] savesymbols;
-
     private static Boolean loaded;
     public static String[] loadsymbols;
-
     int data_block = 100;
-
     public static boolean removeStock = false;
 
  @Override
     public void onCreate(final Bundle savedInstanceState) {  // added final lately
         super.onCreate(savedInstanceState);
-
         loaded = false;
-
         setContentView(R.layout.activity_main);
-
             final EditText input = (EditText) findViewById(R.id.symbol);
             final TextView symbolsList = (TextView) findViewById(R.id.symList);
             symbolsList2 = symbolsList;
@@ -115,7 +96,7 @@ public class Main extends FragmentActivity {
                 public void run() {
                     String text = "One Stock that you've inputted has a wrong Ticker Symbol or may not be a Stock and " +
                             "it has an Average Volume of N/A so replace the Ticker Symbol and Try Again!";
- runOnUiThread(new Runnable() {
+            runOnUiThread(new Runnable() {
                 public void run() {
                     String text = "One Stock that you've inputted has a wrong Ticker Symbol or may not be a Stock and " +
                             "it has an Average Volume of N/A so replace the Ticker Symbol and Try Again!";
@@ -135,16 +116,12 @@ public class Main extends FragmentActivity {
                         Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
                         toast.show();
                     } else {
-                        // symbols = symList2[0].split(",");
-                        // loaded = false;
                         String symList = symList2[0];
                         if (symList == null || symList.trim().equals("")) {
                             String text = "Your Portfolio is empty. Add Stocks first then Get Stock Data before you can use the Refresh Button.";
                             Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
                             toast.show();
                         } else {
-                            //}
-                            //symbols = symList2[0].split(",");
                             int num = symbols.length;
                             String text = "Based on your Portfolio of " + num + " Stocks, the best stock to SELL from your Portfolio is "
                                     + winner + " which has the most positive % price change of " + mostPos +
@@ -366,6 +343,204 @@ public class Main extends FragmentActivity {
                             Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
                             toast.show();
                         }                    
-                    }
-                    }
+                   }
             });
+                 
+            StockAdapter<Stock> adapter = null;            
+            listView = (ListView) findViewById(android.R.id.list);           
+            listView.setAdapter(adapter);       
+            listView.setTextFilterEnabled(true);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                    absolute = position;
+                    if (getResources().getBoolean(R.bool.isPhone)) { //phone //works    
+                        Intent newActivity = new Intent(Main.this, PopUpWindowActivity.class);
+                        startActivity(newActivity);
+                    } else {
+                        Intent newActivity = new Intent(Main.this, PopUpFrags.class); //latest
+                        startActivity(newActivity);
+                    }
+                }
+            });
+        }
+                                          
+            private void initializeAdapter() {
+            StockAdapter<Stock> adapter = null;
+            listView = (ListView) findViewById(android.R.id.list);
+            listView.setAdapter(adapter);    
+                }
+          
+            public void onListSelected(Stock stockItem, int position){
+            Log.d(TAG, "onListSelected: stock = " + stockItem.getName() + "position = " + position);
+            FragmentManager fm = getSupportFragmentManager();
+            listFragment = (ListFragment) fm.findFragmentById(R.id.list_fragment);    
+    }                                 
+     
+     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.addSubMenu("Use XML");
+        menu.addSubMenu("Use JSON");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        mode = item.getItemId();
+        return true;
+    } 
+                                          
+    abstract class BaseStockParser extends AsyncTask<String, Integer, Stock[]> {
+
+        private String makeUrlString(String... symbols) {  
+            StringBuilder sb = new StringBuilder();
+            for (String symbol : symbols) {
+                sb.append(symbol);
+                sb.append("+");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+           
+            String urlStr = "http://finance.yahoo.com/d/quotes.csv?s=" + sb.toString() + "&f=snl1va2p"; //This link used to work
+            // but yahoo seems to have blocked this recently! Is there anyone who can find a workaround around this??
+            
+            return urlStr;
+        }
+ 
+        protected InputStream getData(String[] symbols) throws Exception {
+            InputStream is = null;
+            try  {
+                URL url = new URL(makeUrlString(symbols));                
+                is = url.openStream();           
+            } catch(Exception e) {           
+                String text = "One or more of the Stocks you've inputted has a wrong Crypto Symbol! Try Again!";
+                Toast toast = Toast.makeText(getApplicationContext(), text ,  Toast.LENGTH_LONG);
+                toast.show();
+                e.printStackTrace();
+                }     
+                return is;
+        }
+
+        protected void onPostExecute(Stock[] stocks) {
+            StockAdapter<Stock> adapter =
+                    new StockAdapter<Stock>(Main.this, R.layout.stock, stocks);
+            listView.setAdapter(adapter);   
+        }
+    }
+     
+    private class StockCsvParser extends BaseStockParser {
+        @Override
+        protected Stock[] doInBackground(String... symbols) {
+            Stock[] stocks = new Stock[symbols.length];           
+            List<Stock> stocks2 = null;
+            try {
+                StringBuilder csv = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(getData(symbols)));
+                String line = reader.readLine(); 
+                while (line != null) {
+                    csv.append(line);
+                    csv.append("\n");
+                    line = reader.readLine();
+                }  
+                String[] stocks1 = csv.toString().split("\n");
+                stocks2 = new ArrayList<Stock>(symbols.length);
+                for (String s : stocks1) {
+                    String quotes = ParseCommasInQuotes(s);
+                    String[] values = quotes.split(",");
+                    int vol = 0;
+                    try {
+                        vol = Integer.parseInt(values[3]);
+                    } catch (NumberFormatException e){
+                        Stock stk =  new Stock(values[0],"You've entered a WRONG Ticker Symbol! Try Again!",0.0,0.0,0.0,0.0,true);
+                        stocks2.add(stk);
+                        pubStocks = stocks2.toArray(stocks);
+                        return pubStocks;
+                    }
+                    int avgvol = 0;
+                    try {
+                        avgvol = Integer.parseInt(values[4]);
+                    } catch (NumberFormatException e){
+                         flag2 = true;
+                         Stock stk =  new Stock(values[0],"You've entered a WRONG Ticker Symbol! Try Again!",0.0,0.0,0.0,0.0,true);
+                         stocks2.add(stk);
+                         pubStocks = stocks2.toArray(stocks);
+                         return pubStocks;
+                    } catch (Exception n){
+                        System.out.println("Error(not a NumberFormatException)");
+                    }
+                        flag2 = false;
+                        float pcvol = (float) vol / avgvol;
+                        double pcvold = Math.round(pcvol * 100.0);
+                        double prev = Double.parseDouble(values[5]);
+                        double price = Double.parseDouble(values[2]);
+                        double pchang = (price - prev) / prev * 100;
+                        double pchange = (Math.round(pchang * 100.0)) / 100.0;
+                        boolean flag;
+                        if (pchang >= 0) {
+                            flag = true;
+                        } else {
+                            flag = false;
+                        }
+                        Stock stk = new Stock(values[0], values[1], Double.parseDouble(values[2]), prev, pcvold, pchange, flag);
+                        stocks2.add(stk);
+                        if (loser == null) {
+                            loser = values[1];
+                            mostNeg = pchange;
+                        } else if (mostNeg > pchange) {
+                            loser = values[1];
+                            mostNeg = pchange;
+                        }
+                        if (winner == null) {
+                            winner = values[1];
+                            mostPos = pchange;
+                        } else if (mostPos < pchange) {
+                            winner = values[1];
+                            mostPos = pchange;
+                        }
+                        if (mostAction == null){
+                            mostAction = values[1];
+                            maxAveVol = pcvold;
+                        } else if (maxAveVol < pcvold){
+                            mostAction = values[1];
+                            maxAveVol = pcvold;
+                        }
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                pubStocks = stocks2.toArray(stocks);
+                return pubStocks;
+        }
+    }        
+                                          
+    private String ParseCommasInQuotes(String arg) {
+             boolean foundEndQuote = false;
+             boolean foundStartQuote = false;
+             StringBuilder output = new StringBuilder();
+             //44 = comma
+             //34 = double quote
+             //    foreach (char element in arg)
+             char[] carg = arg.toCharArray();
+             for (char element : carg) {
+                 if (foundEndQuote) {
+                     foundStartQuote = false;
+                     foundEndQuote = false;
+                 }
+                 if ((element == ((char) 34)) & (!foundEndQuote) & foundStartQuote) {
+                     foundEndQuote = true;
+                     continue;
+                 }
+                 if ((element == ((char) 34)) & !foundStartQuote) {
+                     foundStartQuote = true;
+                     continue;
+                 }
+                 if ((element == ((char) 44)) & foundStartQuote) {
+                     //skip comma... its between double quotes
+                 }
+                 else
+                 {
+                     output.append(element);
+                 }
+             }
+             return output.toString();
+         }                                      
